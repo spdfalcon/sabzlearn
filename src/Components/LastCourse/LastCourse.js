ef41f3a3835fac7056ec43b6e1ee09f99e4e1ebd
mohammadreza <spd.mohammadreza@gmail.com>
2023-08-27 02:53:08 -0700

    add coursebox in landing

--- src/Components/LastCourse/LastCourse.js
+++ src/Components/LastCourse/LastCourse.js
@@ -1,18 +1,31 @@
 import React from 'react'
 import './LastCourse.css'
 import SectionHeader from '../SectionHeader/SectionHeader'
+import CourseBox from '../CourseBox/CourseBox'
 export default function LastCourse() {
     return (
         <>
             <div class="courses">
                 <div class="container">
-                    <SectionHeader 
-                    title='جدیدترین دوره ها'
-                    desc='سکوی پرتاب شما به سمت موفقیت'
-                    btnTitle='تمامی دوره ها'
+                    <SectionHeader
+                        title='جدیدترین دوره ها'
+                        desc='سکوی پرتاب شما به سمت موفقیت'
+                        btnTitle='تمامی دوره ها'
                     />
+                    <div class="courses-content">
+                        <div class="container">
+                            <div class="row">
+                                <CourseBox></CourseBox>
+                                <CourseBox></CourseBox>
+                                <CourseBox></CourseBox>
+                                <CourseBox></CourseBox>
+                                <CourseBox></CourseBox>
+                                <CourseBox></CourseBox>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
