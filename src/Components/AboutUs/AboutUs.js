e65059d81387cf4ae425a5fb1707f5d90b17ec4b
mohammadreza <spd.mohammadreza@gmail.com>
2023-08-27 11:32:29 -0700

    create aboutus ui

---
+++ src/Components/AboutUs/AboutUs.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './AboutUs.css'
+import SectionHeader from '../SectionHeader/SectionHeader'
+import AboutUsBox from '../AboutUsBox/AboutUsBox'
+export default function AboutUs() {
+    return (
+        <>
+            <div class="about-us">
+                <div class="container">
+                    <SectionHeader
+                        title='ما چه کمکی بهتون میکنیم'
+                        desc='از اونجایی که آکادمی آموزشی سبزلرن یک آکادمی خصوصی هست'
+                    ></SectionHeader>
+                    <div class="container">
+                        <div class="row">
+                            <AboutUsBox title='دوره های اختصاصی' desc='با پشتیبانی و کیفیت بالا ارائه میده !' icon='fa-copyright'></AboutUsBox>
+
+                            <AboutUsBox title='اجازه تدریس' desc='به هر مدرسی رو نمیده. چون کیفیت براش مهمه !' icon='fa-leaf'></AboutUsBox>
+
+                            <AboutUsBox title='دوره پولی و رایگان' desc='براش مهم نیست. به مدرسینش حقوق میده تا نهایت کیفیت رو در پشتیبانی و اپدیت دوره ارائه بده' icon='fa-gem'></AboutUsBox>
+
+                            <AboutUsBox title='اهمیت به کاربر' desc='اولویت اول و آخر آکادمی آموزش برنامه نویسی سبزلرن اهمیت به کاربرها و رفع نیاز های آموزشی و رسوندن اونها به بازار کار هست' icon='fa-crown'></AboutUsBox>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './Index.css'
 import Header from '../../Components/Header/Header'
 import LastCourse from '../../Components/LastCourse/LastCourse'
+import AboutUs from '../../Components/AboutUs/AboutUs'
 export default function Index() {
   return (
     <div>
       <Header></Header>
       <LastCourse></LastCourse>
+      <AboutUs></AboutUs>
     </div>
   )
 }
