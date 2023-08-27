1183342e02877b9ef758893b01bcad11d0cb0140
mohammadreza <spd.mohammadreza@gmail.com>
2023-08-26 23:46:50 -0700

    Landing ui

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,13 +2,14 @@ import React from 'react'
 import './Header.css'
 import Topbar from '../Topbar/Topbar'
 import Navbar from '../Navbar/Navbar'
+import Landing from '../Landing/Landing'
 export default function Header() {
     return (
         <>
             <header class="header">
                 <Topbar></Topbar>
-
                 <Navbar></Navbar>
+                <Landing></Landing>
             </header>
         </>
     )
