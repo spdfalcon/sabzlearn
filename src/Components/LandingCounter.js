13a573d73acb0e2c7f72b550ab57a63eb4a25a35
mohammadreza <spd.mohammadreza@gmail.com>
2023-08-27 12:13:40 -0700

    Landing Counter

---
+++ src/Components/LandingCounter.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+
+export default function LandingCounter({count}) {
+    const [courseCounter, setCourseCounter] = useState(0)
+
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            setCourseCounter(privCount => privCount + 1)
+        }, 1)
+        if (courseCounter === count) {
+            clearInterval(interval)
+        }
+
+        return () => clearInterval(interval)
+    })
+    return (
+        <span class="landing-status__count">{courseCounter}</span>
+    )
+}
