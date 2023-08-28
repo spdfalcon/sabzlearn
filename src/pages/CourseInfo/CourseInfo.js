b404d2bc5c704073a75163851f5edad6270aa375
mohammadreza <spd.mohammadreza@gmail.com>
2023-08-28 02:38:48 -0700

    create Breadcrumb

--- src/pages/CourseInfo/CourseInfo.js
+++ src/pages/CourseInfo/CourseInfo.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import './CourseInfo.css'
+import Topbar from '../../Components/Topbar/Topbar'
+import Navbar from '../../Components/Navbar/Navbar'
+import Footer from '../../Components/Footer/Footer'
+import Breadcrumb from '../../Components/Breadcrumb/Breadcrumb'
 export default function CourseInfo() {
   return (
-    <div>
-      courseinfo
-    </div>
+    <>
+    <Topbar></Topbar>
+    <Navbar></Navbar>
+    <Breadcrumb
+    links={[
+      {id:1,title:'خانه', to: ''},
+      {id:2,title:'آموزش فرانت اند', to: 'category-info/frontend'},
+      {id:3,title:'دوره متخصص جاوا اسکریپت', to: 'category-info/js-expert'},
+    ]}
+    ></Breadcrumb>
+    <Footer></Footer>
+    </>
   )
 }
