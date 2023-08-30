1cd65fbaa44d5f78df37afa06a42cb5a7810194e
mohammadreza <spd.mohammadreza@gmail.com>
2023-08-30 13:23:27 -0700

    creat CircleSpinner

--- src/Components/CourseBox/CourseBox.js
+++ src/Components/CourseBox/CourseBox.js
@@ -1,48 +1,93 @@
-import React from 'react'
-import './CourseBox.css'
+import React, { useState } from "react";
+import CircleSpinner from "../CircleSpinner/CircleSpinner";
+
+import "./CourseBox.css";
+
 export default function CourseBox() {
-    return (
-        <>
-            <div class="col-4">
-                <div class="course-box">
-                    <a href="#">
-                        <img src="/images/courses/fareelancer.png" alt="Course img" class="course-box__img" />
-                    </a>
-                    <div class="course-box__main">
-                        <a href="#" class="course-box__title">دوره پروژه محور متخصص جنگو</a>
 
-                        <div class="course-box__rating-teacher">
-                            <div class="course-box__teacher">
-                                <i class="fas fa-chalkboard-teacher course-box__teacher-icon"></i>
-                                <a href="#" class="course-box__teacher-link">رضا دولتی</a>
-                            </div>
-                            <div class="course-box__rating">
-                                <img src="/images/svgs/star.svg" alt="rating" class="course-box__star" />
-                                <img src="/images/svgs/star_fill.svg" alt="rating" class="course-box__star" />
-                                <img src="/images/svgs/star_fill.svg" alt="rating" class="course-box__star" />
-                                <img src="/images/svgs/star_fill.svg" alt="rating" class="course-box__star" />
-                                <img src="/images/svgs/star_fill.svg" alt="rating" class="course-box__star" />
-                            </div>
-                        </div>
+  const [isImgShow, setIsImgShow] = useState(false)
 
-                        <div class="course-box__status">
-                            <div class="course-box__users">
-                                <i class="fas fa-users course-box__users-icon"></i>
-                                <span class="course-box__users-text">500</span>
-                            </div>
-                            <span class="course-box__price">1,000,000</span>
-                        </div>
-                    </div>
+  const onImageLoaded = () => setIsImgShow(true)
 
-                    <div class="course-box__footer">
-                        <a href="#" class="course-box__footer-link">
-                            مشاهده اطلاعات
-                            <i class="fas fa-arrow-left course-box__footer-icon"></i>
-                        </a>
-                    </div>
+  const onImageError = () => {
+    // Codes
+  }
 
-                </div>
+  return (
+    <div class="col-4">
+      <div class="course-box">
+        <a href="#">
+          <img
+            src="/images/courses/fareelancer.png"
+            // src="https://placeimg.com/295/295/any/tech?t=190129384"
+            alt="Course img"
+            class="course-box__img"
+            onLoad={onImageLoaded}
+            onError={onImageError}
+          />
+          {
+            !isImgShow && (
+              <CircleSpinner />
+            )
+          }
+        </a>
+        <div class="course-box__main">
+          <a href="#" class="course-box__title">
+            دوره پروژه محور متخصص جنگو
+          </a>
+
+          <div class="course-box__rating-teacher">
+            <div class="course-box__teacher">
+              <i class="fas fa-chalkboard-teacher course-box__teacher-icon"></i>
+              <a href="#" class="course-box__teacher-link">
+                رضا دولتی
+              </a>
             </div>
-        </>
-    )
+            <div class="course-box__rating">
+              <img
+                src="/images/svgs/star.svg"
+                alt="rating"
+                class="course-box__star"
+              />
+              <img
+                src="/images/svgs/star_fill.svg"
+                alt="rating"
+                class="course-box__star"
+              />
+              <img
+                src="/images/svgs/star_fill.svg"
+                alt="rating"
+                class="course-box__star"
+              />
+              <img
+                src="/images/svgs/star_fill.svg"
+                alt="rating"
+                class="course-box__star"
+              />
+              <img
+                src="/images/svgs/star_fill.svg"
+                alt="rating"
+                class="course-box__star"
+              />
+            </div>
+          </div>
+
+          <div class="course-box__status">
+            <div class="course-box__users">
+              <i class="fas fa-users course-box__users-icon"></i>
+              <span class="course-box__users-text">500</span>
+            </div>
+            <span class="course-box__price">1,000,000</span>
+          </div>
+        </div>
+
+        <div class="course-box__footer">
+          <a href="#" class="course-box__footer-link">
+            مشاهده اطلاعات
+            <i class="fas fa-arrow-left course-box__footer-icon"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,16 @@ import Category from "./pages/Caregory/Category"
 import CourseInfo from "./pages/CourseInfo/CourseInfo"
 import Index from "./pages/Index/Index"
 import Courses from "./pages/Courses/Courses"
+import Login from "./pages/Login/Login"
+import Register from "./pages/Register/Register"
 const routes = [
     {path:'/' , element: <Index></Index> },
     {path:'/course-info/:courseName' , element: <CourseInfo></CourseInfo> },
     {path:'/category-info/:categoryName' , element: <Category></Category> },
     {path:'/article-info/:articleName' , element: <ArticleInfo></ArticleInfo> },
     {path:'/courses' , element: <Courses></Courses> },
+    {path:'/Login' , element: <Login></Login> },
+    {path:'/Register' , element: <Register></Register> },
 ]
 
 
