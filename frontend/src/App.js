7ba717092cb2a553e4ba45a1d4a74cbd6ad8222d
mohammadreza <spd.mohammadreza@gmail.com>
2023-09-04 02:00:43 -0700

    use context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import { useRoutes } from 'react-router-dom';
-import routes from './routes';
-function App() {
-  const router = useRoutes(routes)
+import React, { useState } from "react";
+import { useRoutes } from "react-router-dom";
+import AuthContext from "./context/authContext";
+import routes from "./routes";
+
+import "./App.css";
+
+export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(false);
+  const [userInfos, setUserInfos] = useState({});
+
+  const router = useRoutes(routes);
+
+  const login = (token) => {
+    setToken(token)
+    localStorage.setItem('user', JSON.stringify({ token }))
+  }
+
+  const logout = () => {
+    setToken(null)
+    setUserInfos({})
+    localStorage.removeItem('user')
+  }
+
   return (
-    <>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        token,
+        userInfos,
+        login,
+        logout: () => {},
+      }}
+    >
       {router}
-    </>
+    </AuthContext.Provider>
   );
 }
-
-export default App;
